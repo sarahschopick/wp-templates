6ce53d2db19fb7d63f59689d5822eb9f492b6d8b
Sarah Schopick <161715517+sarahschopick@users.noreply.github.com>
2024-08-07 03:58:28 -0700

    Create book-library.php

---
+++ wordpress/wp-content/plugins/book-library/book-library.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Book Library
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     This is a Book Library
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     book-library
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Book_Library
+ */
+
+// Your code starts here.
+require('post-types/books.php');
